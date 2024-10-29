259939761328b0475b84242f1fb90207c84ca627
Adrian Ochs <15180657+adrianochs@users.noreply.github.com>
2024-10-29 08:19:21 +0100

    take interactiveness out

--- nba/config.py
+++ nba/config.py
@@ -1,4 +1,3 @@
-# %%
 import yaml
 from nba.training_models import Config
 from pathlib import Path
@@ -7,8 +6,3 @@ from pathlib import Path
 config_dict = yaml.safe_load((Path(__file__).parent.parent / "config.yaml").read_text())
 
 config = Config.model_validate(config_dict)
-
-# %%
-# print(config.model.model_name)
-
-# %%
